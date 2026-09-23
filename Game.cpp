21d07744c0de6709eb261cffa5c89d41b80795a0
Adam Bożek <adambozek12@gmail.com>
2026-09-23 15:35:44 +0200

    Removed redundant Snake class

--- Game.cpp
+++ Game.cpp
@@ -3,3 +3,48 @@
 //
 
 #include "Game.h"
+
+int Game::newFieldRelativeValue(MoveDirection move) {
+    switch (move) {
+        case MoveDirection::forward:
+            return -30;
+
+        case MoveDirection::backward:
+            return 30;
+
+        case MoveDirection::left:
+            return -1;
+
+        case MoveDirection::right:
+            return 1;
+    }
+    return 0;
+}
+
+Game::Game() {
+    fruit = getIndex(13, 13);
+    snakeInfo = std::make_pair(getIndex(14, 14), getIndex(15, 14));
+    map[getIndex(14, 14)] = true; map[getIndex(15, 14)] = true;
+}
+
+void Game::print() {
+
+}
+
+bool Game::move(MoveDirection direction) {
+    snakeInfo.first += newFieldRelativeValue(direction);
+    map[snakeInfo.first] = true;
+    moveHistory.push(direction);
+
+    if (snakeInfo.first == fruit) {
+        snakeSize++;
+        // and generate a new fruit
+    }
+    else {
+        map[snakeInfo.second] = false;
+        snakeInfo.second += newFieldRelativeValue(moveHistory.front());
+        moveHistory.pop();
+    }
+
+    return true; //will return false if move is invalid - not yet implemented
+}
---
+++ MoveDirection.h
@@ -0,0 +1,17 @@
+//
+// Created by asdab on 23.09.2026.
+//
+
+#ifndef SNAKE_MOVEDIRECTION_H
+#define SNAKE_MOVEDIRECTION_H
+
+
+enum class MoveDirection {
+    forward,
+    backward,
+    left,
+    right
+};
+
+
+#endif //SNAKE_MOVEDIRECTION_H
